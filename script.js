7486f464b53e36c63d2a909ab574e7b72176fb2d
kakashi7860 <sohelshakh91@gmail.com>
2025-12-26 18:58:04 +0530

    hello

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-document.getElementById("submitBtn").addEventListener("click",function (){
+document.getElementById("submitBtn").addEventListener("click", function () {
+
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
 
-    let errorText = document.getElementById("error").value;
-    let successText = document.getElementById("success").value;
+    let errorText = document.getElementById("error");
+    let successText = document.getElementById("success");
 
-    errorText.innerText = " ";
-    successText.innerText = " ";
-    
+    errorText.innerText = "";
+    successText.innerText = "";
 
-    
-
-    if(name === "" && email === ""){
-        errorText.innerText = "All Fields are required"
+    if (name === "" || email === "") {
+        errorText.innerText = "All fields are required";
         return;
     }
 
-    if(!email.includes("@")){
-        errorText.innerText = "please enter a valid email"
+    if (!email.includes("@")) {
+        errorText.innerText = "Please enter a valid email";
+        return;
     }
 
-    successText.innerText = "signup successful !!"
-})
+    successText.innerText = "Signup successful!";
+});
